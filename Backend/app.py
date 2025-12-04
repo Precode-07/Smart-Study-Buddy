05a5db7bd2a66a3af1cf2254088c0e629a1bfc64
Premsagar Sanjaykumar Chauhan <prem6150100@gmail.com>
2025-12-04 18:09:28 +0530

    Update app.py
    Changed the port which will later extended by render it self

--- Backend/app.py
+++ Backend/app.py
@@ -233,4 +233,5 @@ def generate_mcq():
 
 # ----------------- RUN APP -----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
